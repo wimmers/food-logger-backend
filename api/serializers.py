c8e9f061922c03a31d0592b4b644dc42e1e8b785
Simon Wimmer <wimmers@in.tum.de>
2020-12-20 14:48:32 +0100

    refac(serializers) 'code' -> 'product', HyperlinkedModelSerializer -> ModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,19 +3,19 @@ from api.models import Products, ProductToNode, SpottedOn, NotSpottedOn
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
 
 
-class ProductsSerializer(serializers.HyperlinkedModelSerializer):
+class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = ['id',
@@ -40,22 +40,24 @@ class ProductsSerializer(serializers.HyperlinkedModelSerializer):
                   ]
 
 
-class SpottedOnSerializer(serializers.HyperlinkedModelSerializer):
+class SpottedOnSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpottedOn
         fields = ['day', 'product_node_link']
 
 
-class NotSpottedOnSerializer(serializers.HyperlinkedModelSerializer):
+class NotSpottedOnSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotSpottedOn
         fields = ['day', 'product_node_link']
 
 
-class ProductToNodeSerializer(serializers.HyperlinkedModelSerializer):
-    spotted_on = serializers.SlugRelatedField(many=True, read_only=True, slug_field='day')
-    not_spotted_on = serializers.SlugRelatedField(many=True, read_only=True, slug_field='day')
+class ProductToNodeSerializer(serializers.ModelSerializer):
+    spotted_on = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='day')
+    not_spotted_on = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='day')
 
     class Meta:
         model = ProductToNode
-        fields = ['code', 'node', 'created', 'spotted_on', 'not_spotted_on']
+        fields = ['product', 'node', 'created', 'spotted_on', 'not_spotted_on']
